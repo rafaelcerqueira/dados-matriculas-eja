989a060475e59bb0f81320a0c89dae161bb909a7
Rafael Cerqueira <rafaelcerqueiraf@outlook.com>
2023-11-03 15:03:50 -0300

    refatorando os arquivos leitor de dados

--- dados-censo-educacao/Salvador/dados-2021/leitor_de_dados.py
+++ dados-censo-educacao/Salvador/dados-2021/leitor_de_dados.py
@@ -1,8 +1,7 @@
-import os
+#2021
 import pandas as pd
 
-caminho_arquivo_csv = os.path.join('dados', 'matriculas-faixa_etaria-salvador.csv')
-
+url = 'https://raw.githubusercontent.com/rafaelcerqueira/dados-eja-matriculas/main/Salvador'
 class LeitorDeDados:
     def __init__(self, df):
         self.df = df
@@ -10,24 +9,19 @@ class LeitorDeDados:
     
     def get_dados_faixa_etaria_csv(self):
         
-        #df_faixa_etaria = pd.read_csv('/home/lasid-rafael/Documentos/educacao/dados-matriculas-eja/dados-censo-educacao/Salvador/dados-2021/matriculas-faixa_etaria-salvador.csv', sep=';')
-        
-        df_faixa_etaria = pd.read_csv(caminho_arquivo_csv, sep=';')
+        df_faixa_etaria = pd.read_csv(url + '/dados-2021/matriculas-faixa_etaria-salvador.csv', sep=';')
+                                      
         
         return df_faixa_etaria
     
     def get_dados_raca_cor_csv(self):
 
-        df_raca = pd.read_csv('/home/lasid-rafael/Documentos/educacao/dados-matriculas-eja/dados-censo-educacao/Salvador/dados-2021/matriculas-raca-cor-salvador.csv', sep=';')
+        df_raca = pd.read_csv(url + '/dados-2021/matriculas-raca-cor-salvador.csv', sep=';')
         
         return df_raca
     
     def get_dados_sexo_csv(self):
         
-        df_sexo = pd.read_csv('/home/lasid-rafael/Documentos/educacao/dados-matriculas-eja/dados-censo-educacao/Salvador/dados-2021/matriculas-sexo-salvador.csv' , sep=';')
+        df_sexo = pd.read_csv(url + '/dados-2021/matriculas-sexo-salvador.csv', sep=';')
         
         return df_sexo
-
-print(os.getcwd())
-    
-    
