cc6cde91e85b44978e91877dbf74088ba334e567
Rafael Cerqueira <rafaelcerqueiraf@outlook.com>
2023-11-03 23:44:02 -0300

    modulo faixa etaria adicionado em 2018

--- dados-censo-educacao/Salvador/dados-2018/leitor_de_dados.py
+++ dados-censo-educacao/Salvador/dados-2018/leitor_de_dados.py
@@ -1,36 +1,24 @@
-#2022
-
 import pandas as pd
-
-'''
-url = 'https://raw.githubusercontent.com/seu_nome_usuario/seu_repositorio/master/seu_arquivo.csv'
-df = pd.read_csv(url)
-'''
 
 url = 'https://raw.githubusercontent.com/rafaelcerqueira/dados-eja-matriculas/main/Salvador'
 
-
-#Lê os dados dos arquivos csv e os armazena em um dataframe.
 class LeitorDeDados:
-        
+    
     def __init__(self, df):
         self.df = df
-        
+    
     def get_dados_faixa_etaria_csv(self):
-            
-        #df_faixa_etaria = pd.read_csv('/home/lasid-rafael/Documentos/educacao/dados-matriculas-eja/dados-censo-educacao/Salvador/dados-2022/matriculas-faixa_etaria-salvador.csv', sep=';')
-        #df_faixa_etaria = pd.read_csv('/home/rafael/Documents/EJA/dados-matriculas-eja/dados-censo-educacao/Salvador/dados-2022/matriculas-faixa_etaria-salvador.csv', sep=';')
         
         df_faixa_etaria = pd.read_csv(url + '/dados-2022/matriculas-faixa_etaria-salvador.csv', sep=';')
 
         return df_faixa_etaria
-
+    
     def get_dados_raca_cor_csv(self):
-
+            
         df_raca_cor = pd.read_csv(url + '/dados-2022/matriculas-raca-cor-salvador.csv', sep=';')
         
         return df_raca_cor
-
+    
     def get_dados_sexo_csv(self):
 
         df_sexo = pd.read_csv(url + '/dados-2022/matriculas-sexo-salvador.csv')
---
+++ dados-censo-educacao/Salvador/dados-2018/matriculas_faixa_etaria.py
@@ -0,0 +1,160 @@
+import pandas as pd
+
+from leitor_de_dados import LeitorDeDados
+df = pd.DataFrame()
+df = LeitorDeDados(df).get_dados_faixa_etaria_csv()
+
+class MatriculasSalvadorFaixaEtaria:
+    
+    def __init__(self, df):
+        self.df = df
+    
+    def get_matriculas_faixa_etaria(self):
+        
+        matriculas_faixa_etaria = {
+            'Estadual': {
+            
+            },
+            'Municipal': {
+                
+            },
+            'Privada': {
+                
+            },
+            'Federal': {
+                
+            }
+        }
+        
+        for c in range(len(self.df)):
+            if self.df['Categoria 2'][c] == 'Estadual':
+                matriculas_faixa_etaria['Estadual'][self.df['Categoria 1'][c]] = self.df['Matrículas'][c]
+            elif self.df['Categoria 2'][c] == 'Municipal':
+                matriculas_faixa_etaria['Municipal'][self.df['Categoria 1'][c]] = self.df['Matrículas'][c]
+            elif self.df['Categoria 2'][c] == 'Privada':
+                matriculas_faixa_etaria['Privada'][self.df['Categoria 1'][c]] = self.df['Matrículas'][c]
+            elif self.df['Categoria 2'][c] == 'Federal':
+                matriculas_faixa_etaria['Federal'][self.df['Categoria 1'][c]] = self.df['Matrículas'][c]
+                
+        return matriculas_faixa_etaria
+    
+    def get_total_matriculas_faixa_etaria(self):
+        total_matriculas_faixa_etaria = {
+            'Estadual': 0,
+            'Municipal': 0,
+            'Privada': 0,
+            'Federal': 0
+        }
+        
+        for c in range(len(self.df)):
+            if self.df['Categoria 2'][c] == 'Estadual':
+                total_matriculas_faixa_etaria['Estadual'] += self.df['Matrículas'][c]
+            elif self.df['Categoria 2'][c] == 'Municipal':
+                total_matriculas_faixa_etaria['Municipal'] += self.df['Matrículas'][c]
+            elif self.df['Categoria 2'][c] == 'Privada':
+                total_matriculas_faixa_etaria['Privada'] += self.df['Matrículas'][c]
+            elif self.df['Categoria 2'][c] == 'Federal':
+                total_matriculas_faixa_etaria['Federal'] += self.df['Matrículas'][c]
+        
+        return total_matriculas_faixa_etaria
+    
+    def get_total_matriculas_faixa_etaria_porcentagem(self):
+        matriculas_faixa_etaria = self.get_matriculas_faixa_etaria()
+        total_matriculas_faixa_etaria = self.get_total_matriculas_faixa_etaria()
+        
+        total_matriculas_faixa_etaria_porcentagem = {
+            'Estadual': {
+            
+            },
+            'Municipal': {
+                
+            },
+            'Privada': {
+                
+            },
+            'Federal': {
+                
+            }
+        }
+        
+        for c in matriculas_faixa_etaria['Estadual']:
+            total_matriculas_faixa_etaria_porcentagem['Estadual'][c] = round((matriculas_faixa_etaria['Estadual'][c] / total_matriculas_faixa_etaria['Estadual']) * 100, 2)
+        
+        for c in matriculas_faixa_etaria['Municipal']:
+            total_matriculas_faixa_etaria_porcentagem['Municipal'][c] = round((matriculas_faixa_etaria['Municipal'][c] / total_matriculas_faixa_etaria['Municipal']) * 100, 2)
+        
+        for c in matriculas_faixa_etaria['Privada']:
+            total_matriculas_faixa_etaria_porcentagem['Privada'][c] = round((matriculas_faixa_etaria['Privada'][c] / total_matriculas_faixa_etaria['Privada']) * 100, 2)
+        
+        for c in matriculas_faixa_etaria['Federal']:
+            total_matriculas_faixa_etaria_porcentagem['Federal'][c] = round((matriculas_faixa_etaria['Federal'][c] / total_matriculas_faixa_etaria['Federal']) * 100, 2)
+        
+        #adiciona símbolo de porcentagem
+        for c in total_matriculas_faixa_etaria_porcentagem['Estadual']:
+            total_matriculas_faixa_etaria_porcentagem['Estadual'][c] = str(total_matriculas_faixa_etaria_porcentagem['Estadual'][c]) + '%'
+        
+        for c in total_matriculas_faixa_etaria_porcentagem['Municipal']:
+            total_matriculas_faixa_etaria_porcentagem['Municipal'][c] = str(total_matriculas_faixa_etaria_porcentagem['Municipal'][c]) + '%'
+        
+        for c in total_matriculas_faixa_etaria_porcentagem['Privada']:
+            total_matriculas_faixa_etaria_porcentagem['Privada'][c] = str(total_matriculas_faixa_etaria_porcentagem['Privada'][c]) + '%'
+        
+        for c in total_matriculas_faixa_etaria_porcentagem['Federal']:
+            total_matriculas_faixa_etaria_porcentagem['Federal'][c] = str(total_matriculas_faixa_etaria_porcentagem['Federal'][c]) + '%'
+        
+        return total_matriculas_faixa_etaria_porcentagem
+    
+    #somatório de cada faixa etária em todas as dependências administrativas
+    def get_total_matriculas_faixa_etaria_geral(self):
+        matriculas_faixa_etaria = self.get_matriculas_faixa_etaria()
+        total_matriculas_faixa_etaria_geral = {
+            'Até 14 anos': 0,
+            '15 a 17 anos': 0,
+            '18 a 19 anos': 0,
+            '20 a 24 anos': 0,
+            '25 a 29 anos': 0,
+            '30 a 34 anos': 0,
+            '35 a 39 anos': 0,
+            '40 anos ou mais': 0,
+        }
+        
+        for c in matriculas_faixa_etaria['Estadual']:
+            total_matriculas_faixa_etaria_geral[c] += matriculas_faixa_etaria['Estadual'][c]
+        for c in matriculas_faixa_etaria['Municipal']:
+            total_matriculas_faixa_etaria_geral[c] += matriculas_faixa_etaria['Municipal'][c]
+        for c in matriculas_faixa_etaria['Privada']:
+            total_matriculas_faixa_etaria_geral[c] += matriculas_faixa_etaria['Privada'][c]
+        for c in matriculas_faixa_etaria['Federal']:
+            total_matriculas_faixa_etaria_geral[c] += matriculas_faixa_etaria['Federal'][c]
+        
+        return total_matriculas_faixa_etaria_geral
+    
+    #percentual de cada faixa etária.
+    def get_total_matriculas_faixa_etaria_geral_porcentagem(self):
+        matriculas_faixa_etaria = self.get_matriculas_faixa_etaria()
+        total_matriculas_faixa_etaria_geral = self.get_total_matriculas_faixa_etaria_geral()
+        total_matriculas_faixa_etaria_geral_porcentagem = {
+            'Até 14 anos': 0,
+            '15 a 17 anos': 0,
+            '18 a 19 anos': 0,
+            '20 a 24 anos': 0,
+            '25 a 29 anos': 0,
+            '30 a 34 anos': 0,
+            '35 a 39 anos': 0,
+            '40 anos ou mais': 0,
+        }
+        
+        for c in total_matriculas_faixa_etaria_geral:
+            total_matriculas_faixa_etaria_geral_porcentagem[c] = round((total_matriculas_faixa_etaria_geral[c] / sum(total_matriculas_faixa_etaria_geral.values())) * 100, 2)
+        
+        for c in total_matriculas_faixa_etaria_geral_porcentagem:
+            total_matriculas_faixa_etaria_geral_porcentagem[c] = str(total_matriculas_faixa_etaria_geral_porcentagem[c]) + '%'
+        
+        return total_matriculas_faixa_etaria_geral_porcentagem
+        
+            
+
+
+matriculas = MatriculasSalvadorFaixaEtaria(df)
+
+print(matriculas.get_total_matriculas_faixa_etaria_geral_porcentagem())
