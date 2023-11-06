f9de04f4bee43279d69ca855faca0d3626047264
Rafael Cerqueira <rafaelcerqueiraf@outlook.com>
2023-11-06 16:51:59 -0300

    modulos leitor de dados atualizados

--- dados-censo-educacao/Bahia/dados-2016/leitor_de_dados.py
+++ dados-censo-educacao/Bahia/dados-2016/leitor_de_dados.py
@@ -9,19 +9,19 @@ class LeitorDeDados:
 
     def get_dados_faixa_etaria_csv(self):
 
-        df_faixa_etaria = pd.read_csv(url + '/dados-2013/matriculas-faixa_etaria-bahia.csv', sep=';')
+        df_faixa_etaria = pd.read_csv(url + '/dados-2016/matriculas-faixa_etaria-bahia.csv', sep=';')
 
         return df_faixa_etaria
 
     def get_dados_cor_raca_csv(self):
 
-        df_raca_cor = pd.read_csv(url + '/dados-2013/matriculas-cor_raca-bahia.csv', sep=';')
+        df_raca_cor = pd.read_csv(url + '/dados-2016/matriculas-cor_raca-bahia.csv', sep=';')
 
         return df_raca_cor
 
     def get_dados_sexo_csv(self):
 
-        df_sexo = pd.read_csv(url + '/dados-2013/matriculas-sexo-bahia.csv', sep=';')
+        df_sexo = pd.read_csv(url + '/dados-2016/matriculas-sexo-bahia.csv', sep=';')
 
         return df_sexo
 
